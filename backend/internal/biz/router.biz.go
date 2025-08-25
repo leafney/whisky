bfba8c3fc96e4714994c2887b812b8e6dfdba27a
leafney <babycoolzx@126.com>
2025-08-25 20:11:12 +0800

    feat: 优化网络状态监控逻辑

---
+++ backend/internal/biz/router.biz.go
@@ -0,0 +1,7 @@
+package biz
+
+import "github.com/leafney/whisky/pkg/xlogx"
+
+type Router struct {
+	XLog *xlogx.XLogSvc
+}
---
+++ backend/internal/dao/router.dao.go
@@ -0,0 +1,7 @@
+package dao
+
+import "github.com/leafney/whisky/pkg/xlogx"
+
+type Router struct {
+	XLog *xlogx.XLogSvc
+}
